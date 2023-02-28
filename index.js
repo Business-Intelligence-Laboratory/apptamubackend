bd4bd0355d4d83942149426b487ca7851e6be1eb
Arif Roska Perdana <73458415+arifroska@users.noreply.github.com>
2023-02-28 18:57:20 +0700

    fix : port

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.on("listening", console.error.bind(console, "Listening MongoDB:"));
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(tamuRoutes);
 
-app.listen(6000, ()=> console.log('Server up and running...'));
+app.listen(1208, ()=> console.log('Server up and running...'));
