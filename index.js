723852e16eefa113427fdb26cc7eabdeffd12ae5
Arif Roska <arif.roska120820@gmail.com>
2023-07-03 11:22:30 +0700

    cors adjust

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/userRoutes.js";
 import tamuRoutes from "./routes/tamuRoutes.js";
 
 const app = express();
+app.use(cors());
 mongoose.connect('mongodb+srv://lbiftiunandcloud:lbiftiunandcloud@programlab.5p1rj21.mongodb.net/tamuLab', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -13,7 +14,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.on("listening", console.error.bind(console, "Listening MongoDB:"));
 
-// app.use(cors());
+
 app.use(express.json());
 app.use(userRoutes);
 app.use(tamuRoutes);
